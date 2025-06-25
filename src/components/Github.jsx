2760800ab152e595d3a16cfd7795603d9fe79dcd
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-06-25 23:20:22 +0530

    Github Contribution Calendar

---
+++ src/components/Github.jsx
@@ -0,0 +1,39 @@
+import GitHubCalendar from "react-github-calendar";
+
+
+const Github = () => {
+  const theme = {
+    light: [
+      "#ebedf0", // Level 0
+      "#a8f0a2", // Level 1
+      "#70e97c", // Level 2
+      "#42cc61", // Level 3
+      "#21b94a", // Level 4
+    ],
+    dark: [
+      "#161b22", // Level 0 - background gray
+      "#003d1f", // Level 1
+      "#006d32", // Level 2
+      "#26a641", // Level 3
+      "#39d353", // Level 4
+    ],
+  };
+  return (
+    <section>
+
+      <h2 className="my-20 text-center text-4xl">Github Contribution</h2>
+  
+
+      <div className=" flex justify-center p-4 rounded-3xl text-white">
+        <GitHubCalendar
+          username="Avanimaletha"
+          colorScheme="dark"
+          theme={theme}
+        />
+      </div>
+      
+    </section>
+  );
+};
+
+export default Github;
